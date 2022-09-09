2a9ddb0065e3d1447a54f91296a8db50c0223ecb
Ernesto Cardenas <71235388+Ernestocardenas@users.noreply.github.com>
2022-09-09 11:33:16 -0400

    Add files via upload

---
+++ Get-IP.ps1
@@ -0,0 +1,31 @@
+﻿
+$hostnames = Get-Content "C:\Temp\GetIP\hostnames.txt"
+
+$info =@()
+
+
+foreach($device in $hostnames){
+    
+    try{
+
+        $IP = [System.Net.Dns]::GetHostAddresses("$device").IPAddressToString
+
+        $obj = New-Object PSObject
+      
+            $obj | Add-Member -MemberType NoteProperty -Name "Hostname" -Value $device
+            $obj | Add-Member -MemberType NoteProperty -Name "IP" -Value $IP
+
+        $info += $obj
+        }
+    catch{
+        $obj = New-Object PSObject
+      
+            $obj | Add-Member -MemberType NoteProperty -Name "Hostname" -Value $device
+            $obj | Add-Member -MemberType NoteProperty -Name "IP" -Value ""
+
+        $info += $obj
+        }
+
+}
+
+$info | Select * | Export-Csv "C:\Temp\GetIP\655Ips.csv" -NoTypeInformation
